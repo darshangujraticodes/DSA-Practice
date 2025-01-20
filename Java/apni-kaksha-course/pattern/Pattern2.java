5fbd85264520416d5176e6d2f3190fb00f134983
darshangujraticodes <darshangujrati5@gmail.com>
2025-01-20 19:07:47 +0530

    java pattern

---
+++ Java/apni-kaksha-course/pattern/Pattern2.java
@@ -0,0 +1,25 @@
+package pattern;
+public class Pattern2 {
+
+    public void showTrainglePattern(){
+       
+        int count = 5;
+        String str = "";
+
+        for(int i =count; i > 0; i-- ){
+            for(int j = 0; j <i ; j++){
+                str += "*";
+            }
+            str += "\n";
+        }
+        System.out.println(str);
+        
+    }
+    public static void main(String[] args) {
+        
+
+        Pattern2 p1 = new Pattern2();
+        p1.showTrainglePattern();
+
+    }
+}
