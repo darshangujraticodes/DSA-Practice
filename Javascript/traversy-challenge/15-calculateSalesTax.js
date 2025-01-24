8d51ea3a634d24b298842842dbf0ffbdc6fe97fd
darshangujraticodes <darshangujrati5@gmail.com>
2025-01-24 19:10:24 +0530

    js challeneg

---
+++ Javascript/traversy-challenge/15-calculateSalesTax.js
@@ -0,0 +1,21 @@
+const products = [
+  { name: "Apple", price: 0.5, quantity: 10 },
+  { name: "Banana", price: 0.3, quantity: 20 },
+  { name: "Orange", price: 0.6, quantity: 15 },
+];
+
+function calculateTotalSalesWithTax(productsArray, taxAmount) {
+  let temp;
+
+  let sum = 0;
+  for (k of productsArray) {
+    temp = k.price * k.quantity;
+    sum += temp;
+  }
+
+  let finalVal = sum + (sum * taxAmount) / 100;
+
+  console.log(finalVal);
+}
+
+calculateTotalSalesWithTax(products, 8);
