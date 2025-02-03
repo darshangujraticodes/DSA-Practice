b8f98347c0cffa7d076e3a9edce79aa95c159067
darshangujraticodes <darshangujrati5@gmail.com>
2025-02-03 18:48:02 +0530

    java collection

---
+++ Java/JavaPractice/Collections/LearnQueue.java
@@ -0,0 +1,5 @@
+package Java.JavaPractice.Collections;
+
+public class LearnQueue {
+
+}
--- Java/JavaPractice/Collections/LearnStack.java
+++ Java/JavaPractice/Collections/LearnStack.java
@@ -1,5 +1,31 @@
 package Java.JavaPractice.Collections;
 
+import java.util.Stack;
+
 public class LearnStack {
+
+    public static void main(String[] args) {
+
+        Stack<String> bookStack = new Stack<>();
+
+        // push
+        bookStack.push("Physiscs");
+        bookStack.push("Chemistry");
+        bookStack.push("Algebra");
+        bookStack.push("Geometry");
+
+        System.out.println(bookStack);
+
+        // pop()
+
+        bookStack.pop();
+        System.out.println("after pop stack = " + bookStack);
+
+        // isEmpty()
+        System.out.println("isEmpty() = " + bookStack.isEmpty());
+
+        System.out.println("peek() = " + bookStack.peek());
+
+    }
 
 }
