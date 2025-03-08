3cfb0005d842c8811e76e20452df540543321f90
darshangujraticodes <darshangujrati5@gmail.com>
2025-03-08 16:31:25 +0530

    linked list

--- Java/mosh-dsa/linkedlist/Main.java
+++ Java/mosh-dsa/linkedlist/Main.java
@@ -21,10 +21,13 @@
  * 
  */
 
+import java.util.Arrays;
 import java.util.LinkedList;
 
 public class Main {
     public static void main(String[] args) {
+
+        // linkedlist collection
         LinkedList<Integer> ll = new LinkedList<Integer>();
 
         ll.addFirst(10);
@@ -34,14 +37,53 @@ public class Main {
         ll.addLast(50);
         ll.addLast(60);
 
-        System.out.println(ll);
+        // System.out.println(ll);
 
         ll.removeFirst();
         ll.removeLast();
 
         ll.remove(1);
 
-        System.out.println(ll + " size = " + ll.size());
+        // System.out.println(ll + " size = " + ll.size() + " contains = " +
+        // ll.contains(40));
+
+        var array = ll.toArray();
+
+        // System.out.println(Arrays.toString(array));
+
+        // --------------------------------------------
+
+        // custom linkedlist
+
+        var cll = new CustomLinkedList();
+        cll.addLast(10);
+        cll.addLast(20);
+        cll.addLast(30);
+
+        cll.addFirst(-10);
+        cll.addFirst(-20);
+        cll.addFirst(-30);
+
+        cll.remove(10);
+
+        // cll.removeFirst();
+
+        // cll.removeLast();
+
+        cll.print();
+
+        System.out.println("Contains 30 = " + cll.contains(30));
+        // System.out.println("Contains 130 = " + cll.contains(130));
+        // System.out.println("Contains 20 = " + cll.containsCheck(20));
+        System.out.println("Contains 80 = " + cll.containsCheck(80));
+
+        System.out.println("indexof 10 = " + cll.indexOf(10));
+        // System.out.println("indexof 50 = " + cll.indexOf(30));
+        // System.out.println("indexof 80 = " + cll.indexOf(80));
+
+        System.out.println("Linkedlist size = " + cll.size());
+
+        System.out.println("Linkedlist Array = " + Arrays.toString(cll.toArray()));
 
     }
 }
--- Java/notes.md
+++ Java/notes.md
@@ -21,3 +21,36 @@ int marks[] = new int[50];
 int marks = {1,2,3,4,5,6,7,8,9};
 
 ```
+
+#### Pros and Cons
+
+✔️ Static arrays have a fixed size <br>
+✔️ Dyanmic arrays grow by 50-100% <br>
+
+❌ More Memory Usage – Requires extra memory for pointers. <br>
+❌ Slower Access – Cannot access elements directly (like an array); must traverse from the head. <br>
+
+### Linkedlist
+
+1. It is a linear data structure in which elements are stored in memory as separate objects and connected using pointer
+2. It has 2 values data: main content and address : memory address of next node
+3. It has 3 types: single, double and circular
+
+#### Pros and Cons
+
+✔️ Dynamic Size <br>
+✔️ Efficient Insertions/Deletions <br>
+
+❌ More Memory Usage – Requires extra memory for pointers. <br>
+❌ Slower Access – Cannot access elements directly (like an array); must traverse from the head. <br>
+
+| Operation     | Arrays | LinkedList |
+| :------------ | :----: | :--------: |
+| By Index      |  O(1)  |    O(n)    |
+| by Value      |  O(n)  |    O(n)    |
+| insert start  |  O(n)  |    O(1)    |
+| insert end    |  O(1)  |    O(1)    |
+| insert Middle |  O(n)  |    O(n)    |
+| delete start  |  O(n)  |    O(1)    |
+| delete end    |  O(1)  |    O(n)    |
+| delete middle |  O(n)  |    O(n)    |
