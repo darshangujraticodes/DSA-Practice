ba0545f20674b9d870e20f2a85e8e495b80107f9
darshangujraticodes <darshangujrati5@gmail.com>
2025-07-28 18:47:36 +0530

    star pattern

--- Java/apni-kaksha-course/1patterns/StarPatterns.java
+++ Java/apni-kaksha-course/1patterns/StarPatterns.java
@@ -69,11 +69,73 @@ public class StarPatterns {
         System.out.println("");
     }
 
+    public static void hollowRectangle(int n) {
+        System.out.println("Hollow Rectangle Patterns");
+        System.out.println("");
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (i == 1 || i == 4 || j == 1 || j == 4) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println("");
+        }
+        System.out.println("");
+    }
+
+    public static void invertRotatedHalfPyramid(int n) {
+        System.out.println("Inverted Rotated Half Pyramid Patterns");
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (j < (n - i)) {
+                    System.out.print(" ");
+                } else {
+                    System.out.print("*");
+                }
+            }
+            System.out.println("");
+        }
+        System.out.println("");
+    }
+
+    public static void invertedNumberHalfPyramid(int n) {
+        System.out.println("Inverted Number Half Pyramid Patterns");
+        System.out.println();
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(j);
+            }
+            System.out.println("");
+        }
+        System.out.println("");
+    }
+
+    public static void floydTraingle(int n) {
+        System.out.println("Inverted Number Half Pyramid Patterns");
+        System.out.println();
+        int count = 1;
+        for (int i = 0; i <= n; i++) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print(count + " ");
+                count += 1;
+            }
+            System.out.println();
+        }
+        System.out.println("");
+    }
+
     public static void main(String[] args) {
         squarePattern(5);
         rightHalfTraingle(6);
         invertedLeftHalfTraingle(4);
         numberPyramid(5);
         charachterPyramid(5);
+        hollowRectangle(4);
+        invertRotatedHalfPyramid(5);
+        invertedNumberHalfPyramid(5);
+        floydTraingle(4);
     }
 }
